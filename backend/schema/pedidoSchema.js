b7b5e3661e728f50b8f941114b0e672350c80077
GordiGT <efgordillo@ucundinamarca.edu.co>
2026-09-15 12:49:08 -0500

    feat: agregacion de estado "cancelado"

--- backend/schema/pedidoSchema.js
+++ backend/schema/pedidoSchema.js
@@ -24,7 +24,7 @@ const pedidoSchema = new mongoose.Schema({
   },
   estado: {
     type: String,
-    enum: ['Pendiente', 'Comprobado', 'Enviado', 'Entregado'],
+    enum: ['Pendiente', 'Comprobado', 'Enviado', 'Entregado', 'Cancelado'],
     default: 'Pendiente'
   },
   fechaCreacion: {
